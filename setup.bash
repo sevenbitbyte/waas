d1a60991f1035648b602b8f59a7ef77430f96439
Alan Meekins <alan.meekins@gmail.com>
2013-06-07 19:13:16 -0700

    quick setup script

---
+++ setup.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+roscd
+ln -s $OLDPWD/point_downsample/ point_downsample/
+ln -s $OLDPWD/animation_host/ animation_host
+ln -s $OLDPWD/blob_tracker/ blob_tracker/
+ln -s $OLDPWD/ola_dmx_driver/ ola_dmx_driver/
+
+#an ugly script but it'll do
