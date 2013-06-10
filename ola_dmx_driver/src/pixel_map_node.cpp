957929c1184d9ace283b9593be5ab9e79db9a61e
Alan Meekins <alan.meekins@gmail.com>
2013-06-10 07:19:17 -0700

    Implemented dmx rendering and finished implementation of encoding configuration section.

--- ola_dmx_driver/src/pixel_map_node.cpp
+++ ola_dmx_driver/src/pixel_map_node.cpp
@@ -10,6 +10,10 @@
 #include <iniparser.h>
 #include <dictionary.h>
 
+#include <ola/DmxBuffer.h>
+#include <ola/OlaClient.h>
+#include <ola/StreamingClient.h>
+
 ros::NodeHandlePtr _nhPtr;
 
 ros::Publisher _statusPub;
@@ -25,33 +29,34 @@ void imageCallback(const sensor_msgs::Image::ConstPtr& imageMsg);
 //Helper functions
 bool parseConfig(std::string configPath);
 
+#define BYTES_PER_PORT      (512/4)
+#define PIXELS_PER_PORT     (BYTES_PER_PORT/3)
+
+
 struct pixel_map_config{
     uint16_t array_size[2];
 
     struct dmx_map{
-        //int universeId;
         int dmxOffset;
         int imageOffset;
         int pixels;
+        int pixelStep;
     };
 
     map<int, vector<dmx_map> > universes;
 };
 
 pixel_map_config _pixelMap;
+map<int, ola::DmxBuffer*> _dmxBuffers;
+ola::StreamingClient _olaCient(true);
 
 int main(int argc, char** argv){
     ros::init (argc, argv, "point_downsample");
     _nhPtr = ros::NodeHandlePtr(new ros::NodeHandle());
 
-    /*_tfListener = new tf::TransformListener();
-    tf::TransformBroadcaster _broadcaster;
-    _tfBroadcaster = &_broadcaster;*/
-
 
     _frameSub = _nhPtr->subscribe("/animation_host/frame", 1, imageCallback);
 
-    //_statusPub = _nhPtr->advertise<sensor_msgs::PointCloud2> ("/point_downsample/points", 1);
 
 
     //Load settings from ini
@@ -69,6 +74,30 @@ void imageCallback(const sensor_msgs::Image::ConstPtr& imageMsg){
     //Using _pixelMap select mapped regions of image and generate valid DmxBuffer's
     //Use OlaClient to send all DmxBuffer's to correct universes
     //Should use a double buffering approach to avoid visiable tearing
+
+    map<int, ola::DmxBuffer*>::iterator bufferIter = _dmxBuffers.begin();
+
+    //Render into DmuxBuffers
+    for(; bufferIter != _dmxBuffers.end(); bufferIter++){
+        ola::DmxBuffer* buffer = bufferIter->second;
+        vector<pixel_map_config::dmx_map>::iterator mapIter = _pixelMap.universes[ bufferIter->first ].begin();
+
+        for(; mapIter != _pixelMap.universes[ bufferIter->first ].end(); mapIter++){
+            if(mapIter->pixelStep == 3){
+                buffer->SetRange(mapIter->dmxOffset, &imageMsg->data[mapIter->imageOffset], mapIter->pixels * 3);
+            }
+            else{
+                for(int i=0; i < mapIter->pixels; i++){
+                    int pixelIndex = i * mapIter->pixelStep;
+                    buffer->SetRange(mapIter->dmxOffset, &imageMsg->data[mapIter->imageOffset + pixelIndex], 3);
+                }
+            }
+        }
+    }
+
+    for(bufferIter = _dmxBuffers.begin(); bufferIter != _dmxBuffers.end(); bufferIter++){
+        _olaCient.SendDmx(bufferIter->first, *bufferIter->second);
+    }
 }
 
 
@@ -111,9 +140,18 @@ bool parseConfig(std::string configPath){
                     return false;
                 }
 
-                //Check for unsupported configurations
+                //Check for unsupported/non-optimal configurations
                 if(arrayShape != "square"){
                     ROS_WARN("Array shape[%s] not supported! Defaulting to square.", arrayShape.c_str());
+                }
+
+                if(dmxChanDir == "row" ){
+                    ROS_WARN("Setting dmx_chan_dir to value other than col is supported but may not be efficient");
+                }
+                else if(dmxChanDir != "col"){
+                    ROS_ERROR("Unsupported dmx_chan_dir[%s] in pixel map[%s]", dmxChanDir.c_str(), configPath.c_str());
+                    iniparser_freedict(dict);
+                    return false;
                 }
 
                 if(dmxEncoding != "rgb"){
@@ -141,6 +179,8 @@ bool parseConfig(std::string configPath){
                 _pixelMap.array_size[0] = atoi(x.c_str());
                 _pixelMap.array_size[1] = atoi(y.c_str());
 
+                int mappedPorts=0;
+
                 //Read universe listing
                 do{
                     index = universeList.find(',');
@@ -156,13 +196,37 @@ bool parseConfig(std::string configPath){
 
                     int univId = atoi(univIdStr.c_str());
 
-                    vector<pixel_map_config::dmx_map> emptyVec;
-                    _pixelMap.universes.insert(make_pair( univId, emptyVec ));
+                    vector<pixel_map_config::dmx_map> dmxMapVec;
+
+                    if(arrayShape == "square"){
+                        for(int i=0; i<4; i++){
+                            pixel_map_config::dmx_map dmxMap;
+
+                            if(dmxChanDir == "col"){
+                                dmxMap.pixels = _pixelMap.array_size[1];
+                                dmxMap.pixelStep = 3;
+                                dmxMap.dmxOffset = (BYTES_PER_PORT * i);
+                                dmxMap.imageOffset = (mappedPorts * _pixelMap.array_size[1]) * 3;
+                            }
+                            else if(dmxChanDir == "row"){
+                                dmxMap.pixels = _pixelMap.array_size[0];
+                                dmxMap.pixelStep = _pixelMap.array_size[0] * 3;
+                                dmxMap.dmxOffset = (BYTES_PER_PORT * i);
+                                dmxMap.imageOffset = (mappedPorts * _pixelMap.array_size[0]) * 3;
+                            }
+
+                            dmxMapVec.push_back(dmxMap);
+                            mappedPorts++;
+                        }
+                    }
+
+                    _pixelMap.universes.insert(make_pair( univId, dmxMapVec ));
+                    _dmxBuffers.insert(make_pair( univId, new ola::DmxBuffer() ));
 
                 } while(index != string::npos);
             }
             else{
-                //Parse override
+                //Parse override commands
                 ROS_WARN("DMX overrides not yet supported, ignoring section %s.", nodeName.c_str());
             }
         }
