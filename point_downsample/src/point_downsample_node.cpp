daff6c3a83e277c14b4e478fb6ebfb5f7b83fabc
Alan Meekins <alan.meekins@gmail.com>
2013-09-24 23:21:35 -0700

    HsvF

--- point_downsample/src/point_downsample_node.cpp
+++ point_downsample/src/point_downsample_node.cpp
@@ -455,7 +455,7 @@ void updateLights(vector<point3d> centroids){
 
         int value = (int) (fmin(1.0, force[_lightConfig.axis]) );// * 255.0f);
 
-        QColor color = QColor::fromHsv(value, 1.0f, value);
+        QColor color = QColor::fromHsvF(value, 1.0f, value);
 
         //QColor color(value, value, value);
 
