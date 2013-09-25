c8763365f05920bc39cf19a5a4056a05e1936925
Alan Meekins <alan.meekins@gmail.com>
2013-09-24 22:45:04 -0700

    No limit

--- point_downsample/src/point_downsample_node.cpp
+++ point_downsample/src/point_downsample_node.cpp
@@ -448,9 +448,9 @@ void updateLights(vector<point3d> centroids){
         for(int i=0; i<centroids.size(); i++){  //For each blob
             float range = fabs(getDistance(_lightConfig, currentAddress, centroids[i]) / _lightConfig.radius);
 
-            if(range < 1.0f){
+            //if(range < 1.0f){
                 force[_lightConfig.axis] += fmin(1.0f, 1.0f / ( range * range ));
-            }
+            //}
         }
 
         int value = (int) (fmin(1.0, force[_lightConfig.axis]) * 255.0f);
